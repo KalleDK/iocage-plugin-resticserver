f5289a0211e9a53c576e78bcd77c3255a1dc0654
KalleDK <git@k-moeller.dk>
2021-10-01 11:21:34 +0200

    default settings

--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+HOSTNAME=$(hostname)
+
+# Create certificates
+mkdir -p /usr/local/etc/restserver
+openssl req -nodes -x509 -newkey rsa:4096 -keyout /usr/local/etc/restserver/server.key -out /usr/local/etc/restserver/server.crt -days 365 -subj "/CN=${HOSTNAME}"
+chown root:restserver /usr/local/etc/restserver/server.crt /usr/local/etc/restserver/server.key
+chmod o=wr,g=r,o= /usr/local/etc/restserver/server.crt /usr/local/etc/restserver/server.key
+
+# Create htpasswd file
+touch /var/db/restserver/.htpasswd
+chown root:restserver /var/db/restserver/.htpasswd
+chmod o=wr,g=r,o= /var/db/restserver/.htpasswd
+
 # Configure the service
 sysrc restserver_enable=YES
 sysrc restserver_syslog_output_enable=YES
